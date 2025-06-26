ee3cc80adb171b7aee6a6faf914a1af8e08e7793
Fancy Chepngetich Byegon <fancychepngetichbyegon@gmail.com>
2025-06-27 01:54:09 +0300

    Integrate update and delete routes

--- server/app.py
+++ server/app.py
@@ -47,6 +47,29 @@ class PlantByID(Resource):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
 
+    def patch(self, id):
+        plant = Plant.query.get(id)
+        data = request.get_json()
+
+        for attr, value in data.items():
+            setattr(plant, attr, value)
+
+        db.session.commit()
+        return make_response(plant.to_dict(), 200)
+    
+    def delete(self, id):
+        plant = Plant.query.get(id)
+
+        db.session.delete(plant)
+        db.session.commit()
+
+        response = make_response(
+            "",
+            204
+        )
+
+        return response
+
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
